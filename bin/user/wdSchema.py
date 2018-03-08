bc75552756ddbf0e8302456fc994084c0984cf64
gjr80 <gjroderick@gmail.com>
2018-03-08 18:06:01 +1000

    Minor reformatting

--- bin/user/wdSchema.py
+++ bin/user/wdSchema.py
@@ -1,38 +1,40 @@
-##
-##This program is free software; you can redistribute it and/or modify it under
-##the terms of the GNU General Public License as published by the Free Software
-##Foundation; either version 2 of the License, or (at your option) any later
-##version.
-##
-##This program is distributed in the hope that it will be useful, but WITHOUT
-##ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-##FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-##details.
-##
-## Version: 1.2.0b1                                    Date: 1 August 2015
-##
-## Revision History
-##  ?? August 2015      v1.2.0      -removed Weewx-WD schema data from
-##                                   weewxwd3.py
-##
-"""The Weewx-WD schemas"""
-# =============================================================================
-# These lists contain the default schema of the Weewx-WD archive table and the
-# supp table. They are only used for initialization, or in conjunction with the
-# weewxwd_config --create-database and --reconfigure options. Otherwise, once
-# the tables are created the schema is obtained dynamically from the database.
-# Although a type may be listed here, it may not necessarily be supported by
-# your weather station hardware.
+# wdSchema.py
 #
-# You may trim this list of any unused types if you wish, but it will not
-# result in saving as much space as you may think --- most of the space is
-# taken up by the primary key indexes (type "dateTime").
-# =============================================================================
+# The weeWX-WD schema
 #
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+#  Version: 1.2.0a1                                    Date: 8 March 2018
+#
+# Revision History
+#   7 March 2018        v1.2.0
+#       - initial release
+#       - removed weeWX-WD schema data from weewxwd3.py
+#
+"""
+Below are the default schemas for the weeWX-WD archive table and weeWX-WD
+supplementary database supp table. They are only used for initialization, or
+in conjunction with the weewxwd_config --create-database and --reconfigure
+options. Otherwise, once the tables are created the schema is obtained
+dynamically from the database. Although a type may be listed here, it may not
+necessarily be supported by your weather station hardware.
 
-WDSCHEMA_VERSION = '1.2.0b1'
+You may trim this list of any unused types if you wish, but it will not result
+in saving as much space as you may think - most of the space is taken up by the
+primary key indexes (type "dateTime").
+"""
 
-# Define schema for archive table
+WDSCHEMA_VERSION = '1.2.0a1'
+
+# define schema for archive table
 weewxwd_schema = [
     ('dateTime',     'INTEGER NOT NULL UNIQUE PRIMARY KEY'),
     ('usUnits',      'INTEGER NOT NULL'),
@@ -43,7 +45,7 @@ weewxwd_schema = [
     ('outTempNight', 'REAL')
     ]
 
-# Define schema for supp table
+# define schema for supp table
 wdsupp_schema = [
     ('dateTime',            'INTEGER NOT NULL UNIQUE PRIMARY KEY'),
     ('usUnits',             'INTEGER NOT NULL'),
