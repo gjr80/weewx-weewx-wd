77ea7b8a4d0a15267ebd1e21019f1b49143387be
gjr80 <gjroderick@gmail.com>
2019-03-29 11:15:08 +1000

    reformat to remove pycharm complains and add support for renamed stacked wind rose image generator

--- install.py
+++ install.py
@@ -1,56 +1,61 @@
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-#                          Installer for Weewx-WD
-#
-# Version: 1.2.0a1                                      Date: 17 March 2018
-#
-# Revision History
-#   9 March 2018        v1.2.0a1
-#       - weeWX-WD now requires weeWX v3.5.0 or later
-#       - revised Databases and DataBindings config options to support wdSchema
-#         and wd supp database
-#       - added new WdSuppArchive service
-#       - skins now include the enabled config option
-#       - wdPWS skin disabled by default
-#       - added sunshine_threshold config option and Supplementary stanza to
-#         WeeWX-WD config stanza to support weeWX-WD supplementary data
-#       - added new file wdSchema.py
-#       - removed no longer used file wd_database
-#
-# Previous Bitbucket revision history
-#   31 March 2017       v1.0.3
-#       - no change, version number change only
-#   14 December 2016    v1.0.2
-#       - no change, version number change only
-#   30 November 2016    v1.0.1
-#       - minor formatting changes/fix typos
-#       - add weewx version check
-#       - add default units and date settings to weewx.conf as applicable
-#       - added wd_database utility
-#   10 December 2014    v1.0.0a2
-#       - set default HTML_ROOT to WD
-#       - initial cut of readme.txt, added steps to override units, date
-#         format and HTML_ROOT on a per skin basis in weewx.conf
-#   8 December 2014     v1.0.0a1
-#       - initial implementation
-#
+"""
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
 
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+                         Installer for Weewx-WD
+
+Version: 1.2.0a1                                      Date: 29 March 2019
+
+Revision History
+    29 March 2019         v1.2.0a1
+        - WeeWX-WD now requires WeeWX v3.5.0 or later
+        - revised Databases and DataBindings config options to support wdSchema
+          and wd supp database
+        - added new WdSuppArchive service
+        - skins now include the enabled config option
+        - wdPWS skin disabled by default
+        - added sunshine_threshold config option and Supplementary stanza to
+          WeeWX-WD config stanza to support WeeWX-WD supplementary data
+        - added new file wdSchema.py
+        - removed no longer used file wd_database
+        - renamed various install files
+        - reformatted these comments
+
+Previous Bitbucket revision history
+    31 March 2017       v1.0.3
+        - no change, version number change only
+    14 December 2016    v1.0.2
+        - no change, version number change only
+    30 November 2016    v1.0.1
+        - minor formatting changes/fix typos
+        - add weewx version check
+        - add default units and date settings to weewx.conf as applicable
+        - added wd_database utility
+    10 December 2014    v1.0.0a2
+        - set default HTML_ROOT to WD
+        - initial cut of readme.txt, added steps to override units, date
+          format and HTML_ROOT on a per skin basis in weewx.conf
+    8 December 2014     v1.0.0a1
+        - initial implementation
+"""
+
+# python imports
+from distutils.version import StrictVersion
+
+# WeeWX imports
 import weewx
 
-from distutils.version import StrictVersion
 from setup import ExtensionInstaller
 
 REQUIRED_VERSION = "3.5.0"
-WEEWX_WD_VERSION = "1.2.0.a1"
+WEEWX_WD_VERSION = "1.2.0a1"
 
 
 def loader():
@@ -60,14 +65,14 @@ def loader():
 class WeewxWdInstaller(ExtensionInstaller):
     def __init__(self):
         if StrictVersion(weewx.__version__) < StrictVersion(REQUIRED_VERSION):
-            msg = "%s requires weeWX %s or greater, found %s" % ('weeWX-WD ' + WEEWX_WD_VERSION,
+            msg = "%s requires WeeWX %s or greater, found %s" % ('WeeWX-WD ' + WEEWX_WD_VERSION,
                                                                  REQUIRED_VERSION,
                                                                  weewx.__version__)
             raise weewx.UnsupportedFeature(msg)
         super(WeewxWdInstaller, self).__init__(
             version=WEEWX_WD_VERSION,
-            name='Weewx-WD',
-            description='weeWX support for Weather Display Live, SteelSeries Gauges and Carter Lake/Saratoga weather web site templates.',
+            name='WeeWX-WD',
+            description='WeeWX support for Weather Display Live and Carter Lake/Saratoga weather web site templates.',
             author="Gary Roderick",
             author_email="gjroderick<@>gmail.com",
             process_services=['user.weewxwd3.WdWXCalculate'],
@@ -180,17 +185,17 @@ class WeewxWdInstaller(ExtensionInstaller):
                     }
                 },
                 'Weewx-WD': {
-                    'data_binding': 'wd_binding',
+                    'binding': 'wd_binding',
                     'sunshine_threshold': '120',
                     'Supplementary': {
-                        'data_binding': 'wdsupp_binding',
+                        'binding': 'wdsupp_binding',
                         'WU': {
                             'apiKey': 'replace_me'
                         }
                     }
                 }
             },
-            files=[('bin/user', ['bin/user/imageStackedWindRose3.py',
+            files=[('bin/user', ['bin/user/stackedwindrose.py',
                                  'bin/user/wdAstroSearchX3.py',
                                  'bin/user/wdSchema.py',
                                  'bin/user/wdSearchX3.py',
@@ -204,8 +209,6 @@ class WeewxWdInstaller(ExtensionInstaller):
                    ('skins/PWS', ['skins/PWS/weewx_pws.xml.tmpl',
                                   'skins/PWS/skin.conf']),
                    ('skins/StackedWindRose', ['skins/StackedWindRose/skin.conf']),
-                   ('skins/SteelGauges', ['skins/SteelGauges/customclientraw.txt.tmpl',
-                                          'skins/SteelGauges/skin.conf']),
                    ('skins/Testtags', ['skins/Testtags/skin.conf',
                                        'skins/Testtags/testtags.php.tmpl']),
                    ]
